555a60eaa594ee73f3efa820b432f56ea35dc63f
MomoGeeg <mobalde1298@gmail.com>
2026-02-07 02:49:17 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Mon Application React</h1>
+        <p>Bien Venu dans mon application React!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
