1d6a576d2e33558e4cfb1a9f0a8bb8d13fccd0bf
MomoGeeg <mobalde1298@gmail.com>
2026-02-08 04:30:58 +0000

    ✨FO1: LOGIN > Add title and form

--- src/Components/Pages/LoginPage/LoginPage.jsx
+++ src/Components/Pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 export default function LoginPage() {
+  // State (état, données)
+  // const [name, setName] = useState("");
+  const [inputValue, setInputValue] = useState(" ");
+
+  // Comportement (fonction, logique)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bienvenue ${inputValue} !`);
+    setInputValue("");
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div>
       <h1>Bien venu chez Nous !</h1>
+      <h3>Connectez-Vous</h3>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button type="submit">Entrez votre prénom</button>
+      </form>
     </div>
   );
 }
